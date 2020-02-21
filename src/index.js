a6003d80b39991b216ef7a76527a76d3e6793a7c
AlexKif <sashaplakhotniy@gmail.com>
2020-02-21 16:21:50 +0200

    master: create new section

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
-import "./styles/style.scss"
 import {feedback} from "./services/forms";
+import Siema from 'siema';
+import "./styles/style.scss"
 
 const feedbackForm = document.querySelector('.feedback-form');
 const nameField = document.querySelector('#name');
@@ -13,11 +14,42 @@ feedbackForm.onsubmit = (event) => {
     };
     feedback(data)
         .then(() => alert("ок"))
-        .catch(() => alert("не ок"));
-
+        .catch((err) => alert(err));
     nameField.value = '';
     emailField.value = '';
 };
+
+const processItems = document.querySelector('.process__items');
+const processCounter = document.querySelectorAll('.process__counter');
+
+for (let i=1; i<=processItems.children.length; i++) {
+    const test = `${i}`.padStart(3, '0');
+    processCounter[i-1].innerHTML = `${test}`;
+}
+
+document.querySelector('.prev').addEventListener('click', () => mySiema.prev());
+document.querySelector('.next').addEventListener('click', () => mySiema.next());
+
+const mySiema = new Siema({
+    selector: '.siema',
+    duration: 200,
+    easing: 'ease-out',
+    perPage: 1,
+    startIndex: 0,
+    draggable: true,
+    multipleDrag: true,
+    threshold: 20,
+    loop: true,
+    rtl: false,
+});
+
+
+
+
+
+
+
+
 
 
 
